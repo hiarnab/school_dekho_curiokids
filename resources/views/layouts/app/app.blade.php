22389427ad86f263c0e511e3613ca6bbe94a70a7
It's Satyajit💫 <77627641+Its-Satyajit@users.noreply.github.com>
2025-06-02 12:02:46 +0530

    feat: Integrate Debugbar and create home page
    This commit includes the following changes:
    
    1. Integrate the Debugbar package by aliasing the `Debugbar` facade in the `AppServiceProvider`.
    2. Create a new home page view with a basic content section.
    3. Implement a new layout for the application using the `app.blade.php` file.
    4. Remove the default `welcome.blade.php` file.
    
    These changes aim to set up the initial structure and foundation for the application, including the integration of a useful development tool (Debugbar) and the creation of a basic home page.

---
+++ resources/views/layouts/app/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('includes.head')
+
+<body class="bg-slate-100 text-gray-800 scroll-smooth font-figtree dark:bg-black dark:text-white">
+    <main class="max-w-(--breakpoint-xl) mx-auto">
+        @yield('content')
+    </main>
+
+</body>
+
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app.app')
+@section('title', 'Home Page')
+@section('content')
+    <div class="mt-6 gap-4">
+        hi
+    </div>
+
+@endsection
