139a26252db7eee35dc3dd7e552499f679820f51
It's Satyajit💫 <77627641+Its-Satyajit@users.noreply.github.com>
2025-06-04 13:01:19 +0530

    feat(captureForm): Add form for franchise application
    The changes in this commit add a new form for franchise application on the `captureForm.blade.php` file. The form includes fields for the user's full name, mobile number, state, and city. The form is styled using Tailwind CSS classes and includes custom select dropdown functionality using the Headless UI library.

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -127,64 +127,7 @@
                 <p class="py-6"> With India’s Fastest-Growing Preschool Franchise </p>
             </div>
             <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl ml-auto mr-0">
-                <div class="card-body">
-                    <fieldset class="fieldset">
-                        <div class="max-w-sm"> <label for="name"
-                                class="block text-sm font-medium mb-2 dark:text-white">Full Name</label> <input
-                                type="text" id="name"
-                                class="py-2.5 sm:py-3 px-4 block w-full bg-white rounded-lg sm:text-sm focus:border-primary focus:ring-primary focus:ring-priborder-primary disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 border border-gray-200 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-                                placeholder="Your full name" required /> </div>
-                        <div class="max-w-sm"> <label for="mob"
-                                class="block text-sm font-medium mb-2 dark:text-white">Mobile Number</label> <input
-                                type="number" id="mob"
-                                class="py-2.5 sm:py-3 px-4 block w-full bg-white rounded-lg sm:text-sm focus:border-primary focus:ring-primary focus:ring-priborder-primary disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 border border-gray-200 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-                                placeholder="Your full name" required /> </div>
-                        <div class="max-w-sm"> <label for="state"
-                                class="block text-sm font-medium mb-2 dark:text-white">State</label>
-                            <div class="relative"> <select id="state" name="state"
-                                    data-hs-select='{ "placeholder": "Select option...", "toggleTag": "<button type=\"button\" aria-expanded=\falsefalse\"></button>", "toggleClasses": "hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 relative py-3 ps-4 pe-9 flex gap-x-2 text-nowrap w-full cursor-pointer bg-white border border-gray-200 rounded-lg text-start text-sm focus:outline-hidden focus:ring-2 focus:ring-primary dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:outline-hidden dark:focus:ring-1 dark:focus:ring-neutral-600", "dropdownClasses": "mt-2 z-50 w-full max-h-72 p-1 space-y-0.5 bg-white border border-gray-200 rounded-lg overflow-hidden overflow-y-auto dark:bg-neutral-900 dark:border-neutral-700", "optionClasses": "py-2 px-4 w-full text-sm text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-900 dark:hover:bg-neutral-800 dark:text-neutral-200 dark:focus:bg-neutral-800", "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"shrink-0 size-3.5 text-blue-600 dark:text-priborder-primary \" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>" }'>
-                                    <option value="" selected disabled> Select State </option>
-                                    @foreach ($states as $st)
-                                        <option value="{{ $st }}">{{ $st }}</option>
-                                    @endforeach
-                                </select>
-                                <div class="absolute top-1/2 end-2.5 -translate-y-1/2"> <svg
-                                        class="shrink-0 size-4 text-gray-500 dark:text-neutral-500"
-                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round">
-                                        <path d="m7 15 5 5 5-5"></path>
-                                        <path d="m7 9 5-5 5 5"></path>
-                                    </svg> </div>
-                            </div>
-                        </div>
-                        <div class="max-w-sm"> <label for="city"
-                                class="block text-sm font-medium mb-2 dark:text-white">City</label>
-                            <div class="relative"> <select id="city" name="city"
-                                    data-hs-select='{ "placeholder": "Select option...", "toggleTag": "<button type=\"button\" aria-expanded=\falsefalse\"></button>", "toggleClasses": "hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 relative py-3 ps-4 pe-9 flex gap-x-2 text-nowrap w-full cursor-pointer bg-white border border-gray-200 rounded-lg text-start text-sm focus:outline-hidden focus:ring-2 focus:ring-primary dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:outline-hidden dark:focus:ring-1 dark:focus:ring-neutral-600", "dropdownClasses": "mt-2 z-50 w-full max-h-72 p-1 space-y-0.5 bg-white border border-gray-200 rounded-lg overflow-hidden overflow-y-auto dark:bg-neutral-900 dark:border-neutral-700", "optionClasses": "py-2 px-4 w-full text-sm text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-900 dark:hover:bg-neutral-800 dark:text-neutral-200 dark:focus:bg-neutral-800", "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"shrink-0 size-3.5 text-blue-600 dark:text-priborder-primary \" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>" }'>
-                                    <option value="" selected disabled> Select State </option>
-                                    @foreach ($states as $st)
-                                        <option value="{{ $st }}">{{ $st }}</option>
-                                    @endforeach
-                                </select>
-                                <div class="absolute top-1/2 end-2.5 -translate-y-1/2"> <svg
-                                        class="shrink-0 size-4 text-gray-500 dark:text-neutral-500"
-                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round">
-                                        <path d="m7 15 5 5 5-5"></path>
-                                        <path d="m7 9 5-5 5 5"></path>
-                                    </svg> </div>
-                            </div>
-                        </div> <small class="text-xm mt-2 text-gray-500 dark:text-neutral-500"> By clicking on submit,
-                            you agree to our <a href="#" class="text-blue-600 hover:underline">Terms of
-                                Service</a> and <a href="#" class="text-blue-600 hover:underline">Privacy
-                                Policy</a>. </small>
-                    </fieldset>
-                    <div class=""> <button type="submit"
-                            class="btn btn-secondary w-full uppercase font-semibold tracking-wider"> Submit </button>
-                    </div>
-                </div>
+                @include('sections/forms/captureForm')
             </div>
         </div>
     </div>
@@ -292,9 +235,49 @@
             </div>
         </div>
         <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6  justify-center">
-            <button class="btn  max-w-sm  btn-primary w-full uppercase font-semibold tracking-wider">Apply Now <i
-                    class="ti ti-arrow-narrow-right"></i>
+            {{-- <button class="btn  max-w-sm  btn-primary w-full uppercase font-semibold tracking-wider">Apply Now 
+            </button> --}}
+
+            <button type="button"
+                class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                aria-haspopup="dialog" aria-expanded="false" aria-controls="hs-vertically-centered-modal"
+                data-hs-overlay="#hs-vertically-centered-modal">
+                Apply Now <i class="ti ti-arrow-narrow-right"></i>
             </button>
+
+            <div id="hs-vertically-centered-modal"
+                class="hs-overlay  hidden size-full fixed top-0 start-0 z-80 overflow-x-hidden overflow-y-auto pointer-events-none ralative"
+                role="dialog" tabindex="-1" aria-labelledby="hs-vertically-centered-modal-label">
+                <div
+                    class="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto min-h-[calc(100%-56px)] flex items-center">
+                    <div
+                        class="w-full flex flex-col max-w-sm bg-white border border-gray-200 shadow-2xs rounded-xl pointer-events-auto dark:bg-neutral-800 dark:border-neutral-700 dark:shadow-neutral-700/70">
+
+                        {{-- <h3 id="hs-vertically-centered-modal-label"
+                                class="font-bold text-gray-800 dark:text-white">
+                                Modal title
+                            </h3> --}}
+                        <div class="relative max-w-sm">
+                            @include('sections/forms/captureForm')
+                            <div class=" absolute top-2 right-2">
+                                <button type="button"
+                                    class="size-8 inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-hidden focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:hover:bg-neutral-600 dark:text-neutral-400 dark:focus:bg-neutral-600"
+                                    aria-label="Close" data-hs-overlay="#hs-vertically-centered-modal">
+                                    <span class="sr-only">Close</span>
+                                    <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24"
+                                        height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                        <path d="M18 6 6 18"></path>
+                                        <path d="m6 6 12 12"></path>
+                                    </svg>
+                                </button>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
